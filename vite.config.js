d14ec86878923c13ef398b93f28b8fe3ffa77c32
amrit-224236794 <s224236794@deakin.edu.au>
2024-09-18 10:54:00 +1000

    updated search

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'], // example for common vendor libraries
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // adjust if necessary
+  },
+});
